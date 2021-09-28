1fef5ca056463671db66bed22b11c1cd742444aa
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2021-09-28 10:59:18 -0700

    Add files via upload

--- Node.js
+++ Node.js
@@ -45,10 +45,41 @@ class DoublyLinkedList {
         }
         this.length--;
     }
+    shift() {
+        if(!this.head) return null;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+        var oldHead = this.head;
+        this.head = oldHead.next;
+        this.head.previous = null;
+        oldHead.next = null;
+        }
+        this.length--;
+    }
+    unshift(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+        
+            this.head.previous = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return list;
+
+    }
 }
 var list = new DoublyLinkedList();
 list.push("Frankie");
 list.push("Hussein");
 list.push("Cerina");
-list.pop();
+list.push("Andrea");
+list.shift();
+list.unshift("Brenda");
 list.traverse();
+console.log(list)
